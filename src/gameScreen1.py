2964390211ae33832f71ac2c8a0f45c195cfbd5e
jdh0001 <jdh0001@mix.wvu.edu>
2019-04-24 21:49:56 -0400

    added sounds but commented them out. going to see if the path will recognize them after I push.

--- src/gameScreen1.py
+++ src/gameScreen1.py
@@ -125,15 +125,29 @@ class Sprite:
             if(leftX > -450):
                 screen.blit(pygame.transform.scale(self.hellbender, (400, 150)), (leftX, 450))
                 leftX -= 45
+                
+              
+#         effect = pygame.mixer.Sound('../assets/hooked.mp3')
 
+        
         # BEWARE THE HOOKS! THESE KILL THE PLAYER!!!!!!!!!!!!!!
         # (In order from left most hook to right most hook)
         if (self.x > -800 and self.x < -125 and self.y > -2350 and self.y < -2050):
+#             effect.play()
+#             effect.stop()
             air = 5050
+            
         if (self.x > -1200 and self.x < -525 and self.y > -3125 and self.y < -2850):
+#             effect.play()
+#             effect.stop()
             air = 5050
+            
         if (self.x > -4575 and self.x < -3900 and self.y > -2750 and self.y < -2475):
+#             effect.play()
+#             effect.stop()
             air = 5050
+            
+            
 
 def game1(screen, playerGender):
     inventoryItems = [0, 0, 0, 0, 0, 0, 0]
